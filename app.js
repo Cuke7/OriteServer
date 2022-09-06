4d2e51219b05cbcfea09b501131a2020b8f90aea
Cuke7 <cuke.dev@mail.com>
2022-09-06 17:38:56 +0200

    test

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ const express = require("express");
 const app = express();
 const ytdl = require("ytdl-core");
 const rp = require('request-promise')
-const rq = require('request')
 
 app.listen(process.env.PORT || 8080, () => {
     console.log("Listening to requests on 8080");
 });
 
-const sendSeekable = require("send-seekable");
-app.use(sendSeekable);
-app.get('/getStream', getStream);
 app.get('/getPlaylist', getPlaylist);
+app.get("/getUrl", getUrl)
+
 
 async function getPlaylist(req, resp) {
     if (decodeURI(req.query.url.length) > 0) {
@@ -46,37 +44,9 @@ async function getPlaylist(req, resp) {
 }
 
 
-
-async function getStream(req, res) {
+async function getUrl(req, res) {
     let id = decodeURI(req.query.id);
-    let url = "https://www.youtube.com/watch?v=" + id;
-
     let info = await ytdl.getInfo(id);
-
-    let format = ytdl.chooseFormat(info.formats, { quality: "lowestaudio" });
-    let type = "audio/mpeg";
-    let size = format.contentLength;
-
-    let stream = ytdl(url, {
-        format: format,
-        requestOptions: {
-            headers: DEFAULT_HEADERS,
-        },
-    });
-
-    res.sendSeekable(stream, {
-        type: type,
-        length: size,
-    });
+    let format = ytdl.chooseFormat(info.formats, { quality: "highestaudio" });
+    res.json(format.url);
 }
-
-const DEFAULT_HEADERS = {
-    "User-Agent": getFirefoxUserAgent(),
-    "Accept-Language": "en-US,en;q=0.5",
-};
-function getFirefoxUserAgent() {
-    let date = new Date();
-    let version = (date.getFullYear() - 2018) * 4 + Math.floor(date.getMonth() / 4) + 58 + ".0";
-    return `Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:${version} Gecko/20100101 Firefox/${version}`;
-}
-
